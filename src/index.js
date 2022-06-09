fff5cacdef91991a6066f029ae3fb994ff1070a5
Michał <michaldziuba.praca@gmail.com>
2022-06-09 20:53:27 +0200

    Update

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter,Route,Routes } from "react-router-dom";
+import { BrowserRouter,HashRouter,Route,Routes } from "react-router-dom";
 import { NotFound } from "./Components/NotFound/NotFound";
 import {HomePage} from "./Components/HomePage/HomePage";
 import { SearchPage } from "./Components/SearchPage/SearchPage";
@@ -13,21 +13,23 @@ import { BookPage } from "./Components/BookPage/BookPage";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
-    <Suspense fallback="Please wait a second">
-      <Routes>
-        <Route path="/" element={<App />}>
-          <Route index element={<HomePage />} />
-          <Route path="/search" element={<SearchPage />}>
-            <Route path=":bookId" element={<BookPage />} />
-          </Route>
-          <Route path="/favorites" element={<Favorites />} />
+  <React.StrictMode>
+    <HashRouter>
+      <Suspense fallback="Please wait a second">
+        <Routes>
+          <Route path="/" element={<App />}>
+            <Route index element={<HomePage />} />
+            <Route path="/search/" element={<SearchPage />}>
+              <Route path=":bookId" element={<BookPage />} />
+            </Route>
+            <Route path="/favorites/" element={<Favorites />} />
 
-          <Route path="*" element={<NotFound />} />
-        </Route>
-      </Routes>
-    </Suspense>
-  </BrowserRouter>
+            <Route path="*" element={<NotFound />} />
+          </Route>
+        </Routes>
+      </Suspense>
+    </HashRouter>
+  </React.StrictMode>
 );
 
 
